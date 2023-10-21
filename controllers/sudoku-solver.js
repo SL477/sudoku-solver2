60e7308c9d2f2c7ecaaf5ab1b6a4e1a62a67b98c
Tom Fishwick <fishwickt@gmail.com>
2023-10-21 19:00:27 +0100

    Got it to solve sudokus recursively

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,11 +1,18 @@
 class SudokuSolver {
+  /**
+   * Check that the cell is in 1-9
+   * @param input{string}
+   * @returns{bool}
+   */
   isInputValid(input) {
-    //Myfunction
+    //MyFunction
+    // TODO sort this
     const zeroToNine = ['1','2','3','4','5','6','7','8','9'];
     return zeroToNine.includes(input);
   }
 
   validate(puzzleString) {
+    // TODO sort this
     const validChars = ['1','2','3','4','5','6','7','8','9', '.'];
     if (puzzleString.length !== 81) {
       return { error: 'Expected puzzle to be 81 characters long' };
@@ -18,24 +25,48 @@ class SudokuSolver {
     return true;
   }
 
+  /**
+   * Get which panel an index belongs to
+   * @param index{number}
+   * @returns{number}
+   */
   getBoxNumber(index) {
-    let boxNums = [0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8];
-    return boxNums[index];
+    const boxNumbers = [0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8];
+    return boxNumbers[index];
   }
 
+  /**
+   * Convert a row to a number
+   * @param row{string} Needs to be A-I
+   * @returns{number}
+   */
   getRowNumber(row) {
-    let rows = ['A','B','C','D','E','F','G','H','I'];
-    let rowIndex = rows.findIndex((r) => {return r === row});
-    return rowIndex;
+    const rows = ['A','B','C','D','E','F','G','H','I'];
+    return rows.findIndex((r) => {return r === row});
   }
 
+  /**
+   * Convert a column number to a number
+   * @param col{string} Needs to be 1-9
+   * @returns{number}
+   */
   getColumnNumber(col) {
-    let cols = ['1','2','3','4','5','6','7','8','9'];
-    let colIndex = cols.findIndex((r) => {return r === col});
-    return colIndex;
+    const cols = ['1','2','3','4','5','6','7','8','9'];
+    return cols.findIndex((r) => {return r === col});
   }
 
+  /**
+   * Convert to row and column
+   * @param value{string}
+   * @returns{{
+    row: null;
+    column: null;
+} | {
+    error: string;
+}}
+   */
   getRowColumn(value) {
+    // TODO sort out
     //Value in form of A1-I9
     if (!value) {
       return {error: 'Invalid coordinate'};
@@ -60,7 +91,14 @@ class SudokuSolver {
     return ret;
   }
 
+  /**
+   * Get the possibilities for a row
+   * @param row{number} The row to check
+   * @param puzzle{string} The puzzle
+   * @returns{string[]}
+   */
   getPossibilitiesForRow(row, puzzle) {
+    // TODO sort out
     let ret = ['1','2','3','4','5','6','7','8','9'];
     for (var i = 0; i < 9; i++) {
       let number = puzzle[i + (9 * row)];
@@ -72,14 +110,30 @@ class SudokuSolver {
     return ret;
   }
 
+  /**
+   * Check what possibilities are allowed in a row
+   * @param puzzleString{string}
+   * @param row{number}
+   * @param column{number}
+   * @param value{number}
+   * @returns{boolean}
+   */
   checkRowPlacement(puzzleString, row, column, value) {
+    //TODO is this used? (used in tests), should it be used elsewhere
     //Returns true/false
-    let possRow = this.getPossibilitiesForRow(row, puzzleString);
-    return possRow.includes(String(value));
+    const possibilitiesInRow = this.getPossibilitiesForRow(row, puzzleString);
+    return possibilitiesInRow.includes(String(value));
   }
 
+  /**
+   * Allowed values in column
+   * @param col{number}
+   * @param puzzle{string}
+   * @returns{string[]}
+   */
   getPossibilitiesForColumn(col, puzzle) {
-    let ret = ['1','2','3','4','5','6','7','8','9'];
+    //todo sort
+    const ret = ['1','2','3','4','5','6','7','8','9'];
     for (var i = 0; i < 9; i++) {
       let number = puzzle[col + (9 * i)];
       //console.log('number',number)
@@ -92,17 +146,33 @@ class SudokuSolver {
     return ret;
   }
 
+  /**
+   * Check that the value is allowed in the column
+   * @param puzzleString{string}
+   * @param row{number}
+   * @param column{number}
+   * @param value{number}
+   * @returns{boolean}
+   */
   checkColPlacement(puzzleString, row, column, value) {
-    let possCol = this.getPossibilitiesForColumn(column, puzzleString);
-    return possCol.includes(String(value));
+    //todo is this used, should it be
+    const possibilitiesCol = this.getPossibilitiesForColumn(column, puzzleString);
+    return possibilitiesCol.includes(value.toString());
   }
 
-  getPossibilitiesForBox(num, input) {
+  /**
+   * Get the allowed values for the box
+   * @param num{number} The box index
+   * @param puzzleString{string}
+   * @returns{string[]}
+   */
+  getPossibilitiesForBox(num, puzzleString) {
+    // todo sort out
     let boxStarts = [0,3,6,27,30,33,54,57,60];
     let offset = [0,1,2,9,10,11,18,19,20];
     let ret = ['1','2','3','4','5','6','7','8','9'];
     for (var i = 0; i < 9; i++) {
-      let number = input[boxStarts[num] + offset[i]];
+      let number = puzzleString[boxStarts[num] + offset[i]];
       //console.log('number',number)
       if (this.isInputValid(number)) {
         let index = ret.indexOf(number);
@@ -113,23 +183,40 @@ class SudokuSolver {
     return ret;
   }
 
+  /**
+   * Check the value in the region
+   * @param puzzleString{string}
+   * @param row{number}
+   * @param column{number}
+   * @param value{number}
+   * @returns{boolean}
+   */
   checkRegionPlacement(puzzleString, row, column, value) {
+    // todo sort out
     let index = (row * 9) + column;
     let boxNum = this.getBoxNumber(index);
-    let possBox = this.getPossibilitiesForBox(boxNum, puzzleString);
-    return possBox.includes(String(value));
+    let possibilitiesBox = this.getPossibilitiesForBox(boxNum, puzzleString);
+    return possibilitiesBox.includes(String(value));
   }
 
-  getPossibilitiesForCell(index, input) {
-    if (this.isInputValid(input[index])) {
+  /**
+   * Get the allowed values for a cell
+   * @param index{number}
+   * @param puzzleString{string}
+   */
+  getPossibilitiesForCell(index, puzzleString) {
+    // TODO sort out
+    if (this.isInputValid(puzzleString[index])) {
       return [];
     }
-    let columnPoss = this.getPossibilitiesForColumn(index % 9, input);
-    let rowPoss = this.getPossibilitiesForRow(Math.floor(index / 9), input);
-    let boxPoss = this.getPossibilitiesForBox(this.getBoxNumber(index), input);
+    const columnPossibilities = this.getPossibilitiesForColumn(index % 9, puzzleString);
+    const rowPossibilities = this.getPossibilitiesForRow(Math.floor(index / 9), puzzleString);
+    const boxPossibilities = this.getPossibilitiesForBox(this.getBoxNumber(index), puzzleString);
   
-    let arr = [columnPoss, rowPoss, boxPoss];
-    return arr.reduce((p,c) => p.filter(e => c.includes(e)));
+    //let arr = [columnPossibilities, rowPossibilities, boxPossibilities];
+    //return arr.reduce((p,c) => p.filter(e => c.includes(e)));
+    const mergeColumnAndRow = columnPossibilities.filter(value => rowPossibilities.includes(value))
+    return mergeColumnAndRow.filter(value => boxPossibilities.includes(value));
   }
 
   isThereCellWithOnePossibility(input) {
@@ -160,7 +247,7 @@ class SudokuSolver {
       let num = this.getPossibilitiesForCell(indexOfCellWithOnePossibility, ret);
       ret = this.setNumberInInput(num[0], indexOfCellWithOnePossibility, ret);
       indexOfCellWithOnePossibility = this.isThereCellWithOnePossibility(ret);
-      console.log('new puzzle', ret);
+      //console.log('new puzzle', ret);
     }
     return ret;
   }
@@ -228,6 +315,11 @@ class SudokuSolver {
     return true;
   }
 
+  /**
+   * Get the next free cell
+   * @param input{string} The sudoku puzzle
+   * @returns{number}
+   */
   getNextUnsolvedCell(input) {
     for (var i = 0; i < 81; i++) {
       if (!this.isInputValid(input[i])) {
@@ -237,6 +329,53 @@ class SudokuSolver {
     return -1;
   }
 
+  /**
+   * This is to recursively solve it
+   * It needs to run through guessing the next number
+   * @param input{string}
+   * @returns{string[]|bool}
+   */
+  recursiveSolve(input) {
+    let nextUnsolvedCell = this.getNextUnsolvedCell(input);
+    if (nextUnsolvedCell == -1) {
+      return input;
+    }
+
+    let newInput = [...input];
+
+    // for (let i = 1; i < 10; i++) {
+    //   newInput[nextUnsolvedCell] = i;
+    //   if (this.isPuzzleValid(newInput)) {
+    //     let newPuzzle = this.recursiveSolve(newInput);
+    //     if (newPuzzle) {
+    //       return newPuzzle;
+    //     }
+    //   }
+    // }
+    const cellPossibilities = this.getPossibilitiesForCell(nextUnsolvedCell, newInput)
+    for (let i = 0; i < cellPossibilities.length; i++) {
+      newInput[nextUnsolvedCell] = cellPossibilities[i];
+      if (this.isPuzzleValid(newInput)) {
+        let newPuzzle = this.recursiveSolve(newInput);
+        if (newPuzzle) {
+          return newPuzzle;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * This is to try to solve the sudoku puzzle
+   * @param puzzleString{string} The sudoku puzzle with blanks as .
+   * @returns{{
+    error: string;
+    solution?: undefined;
+} | {
+    solution: any;
+    error?: undefined;
+}}
+   */
   solve(puzzleString) {
     if (!this.isPuzzleValid(puzzleString)) {
       return { error: 'Puzzle cannot be solved' };
@@ -248,6 +387,10 @@ class SudokuSolver {
     }
 
     // otherwise we need to start solving it recursively
+    newSolution = this.recursiveSolve(newSolution);
+    if (newSolution) {
+      return { solution: newSolution.join('') };
+    }
 
     return { error: 'Puzzle cannot be solved' };
   }
